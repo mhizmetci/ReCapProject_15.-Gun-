bdf6438d69341e95767457293de2e6721d3d616f
mhizmetci <mhizmetci@gmail.com>
2021-02-23 01:06:02 +0300

    17th February, The 12th Live Lesson, ReCap Project including AOP

---
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,27 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.ValidationRules.FluentValidation
+{
+   public class UserValidator: AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(u => u.FirstName).NotEmpty();
+            RuleFor(u => u.LastName).NotEmpty();
+            RuleFor(u => u.Email).NotEmpty();
+            RuleFor(u => u.Email).Must(Contain).WithMessage("Lütfen Geçerli bir email adresi giriniz !");
+            RuleFor(u => u.Password).NotEmpty();
+            RuleFor(u => u.Password).MinimumLength(8);
+
+        }
+
+        private bool Contain(string arg)
+        {
+            return arg.Contains("@");
+        }
+    }
+}
